fa14adcf923af2cf1e5720b9c85ae13332800532
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2024-03-05 15:00:19 +0300

    APPIUM

--- src/test/java/tests/KiwiApp.java
+++ src/test/java/tests/KiwiApp.java
@@ -9,5 +9,18 @@ public class KiwiApp {
     @Test
     public void kiwiAppTest(){
 
+        // uygulamanin yuklendigi dogrulanir
+        // uygulamanin basariyla acildigi dogrulanir
+        // misafir olarak devam et e tiklanir
+        // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
+        // Trip type,one way olarak secilir
+        // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+        // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
+        // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
+        // gidis tarihi eylul ayinin 21 i olarak secilir ve set date e tiklanir
+        // search butonuna tiklanir
+        // en  ucuz ve aktarmasiz filtrelemeleri yapilir
+        // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
+
     }
 }
