2eeb00f1fb43730a2644cc28050cfbc925dc26e0
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2024-03-07 13:21:29 +0300

    Appium

--- src/test/java/tests/AllCurrencyConverterApp.java
+++ src/test/java/tests/AllCurrencyConverterApp.java
@@ -2,28 +2,57 @@ package tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Driver;
+import utils.ResuableMethods;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AllCurrencyConverterApp {
-    AndroidDriver<AndroidElement>driver= Driver.getAndroidDriver();
+    AndroidDriver<AndroidElement> driver = Driver.getAndroidDriver();
 
     @Test
-    public void allCurrencyTest(){
+    public void allCurrencyTest() throws InterruptedException, IOException {
 
         // all currency uygulamasinin yuklendigi dogulanir
-        Assert.assertTrue(driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter.CurrencyConverter"));
-        // uygulamanin acildigi dogrulanir
-        // cevirmek istedigimiz para birimi zloty olarak secilir
-        // cevirelecek olan para birimi Tl olarak secilir
-        // cevrilen tutar screenShot olarak kaydedilir
-        // Ardindan zloty nin tl karsiligi olan tl degeri kaydedilir
-        // kullaniciya sms olarak bildirilir
+        Assert.assertTrue(driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter"));
 
+        // uygulamanin acildigi dogrulanir
+        Assert.assertTrue(driver.findElementByXPath("//*[@text='CURRENCY CONVERTER']").isDisplayed());
+
+        // cevirmek istedigimiz para birimi zloty olarak secilir
+        ResuableMethods.koordinatTiklamaMethodu(435, 347, 1500);
+        ResuableMethods.scrollWithUiScrollable("Polish Zloty");
+
+        // cevirelecek olan para birimi Tl olarak secilir
+        ResuableMethods.koordinatTiklamaMethodu(424, 482, 1500);
+        ResuableMethods.scrollWithUiScrollable("Turkish Lira");
+
+        // cevrilen tutar screenShot olarak kaydedilir
+        ResuableMethods.scrollWithUiScrollable("1");
+        ResuableMethods.scrollWithUiScrollable("5");
+        ResuableMethods.scrollWithUiScrollable("3");
+        ResuableMethods.scrollWithUiScrollable("8");
+
+        /*
+        File file = driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file,new File("ExchangeRateForZlotyToTl.jpg"));
+
+         */
+
+        ResuableMethods.getScreenshot("Ugur");
+        // Ardindan zloty nin tl karsiligi olan tl degeri kaydedilir
+        AndroidElement exchangeResult= driver.findElementById("com.smartwho.SmartAllCurrencyConverter:id/EditTextCurrencyB");
+        String sonucText=exchangeResult.getText();
+
+        // kullaniciya sms olarak bildirilir
+        driver.sendSMS("55555555555","Cevirmek istediginiz para biriminizin sonucu = "+sonucText);
 
     }
-
 
 
 }
